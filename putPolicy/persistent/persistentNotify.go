04f39170d16258dbe04df0793e65c7ef2d149b9c
Jerry Bendy <jerry@icewingcc.com>
2019-08-21 11:31:58 +0800

    移除部分无效代码

--- putPolicy/persistent/persistentNotify.go
+++ putPolicy/persistent/persistentNotify.go
@@ -6,9 +6,7 @@ type NotifyRequestBody struct {
 }
 
 type Result struct {
-	Code           int    `json:"code"`         // 状态码。0 表示成功；1 表示失败
-	Desc           string `json:"desc"`         // 状态对应的描述
-	PersistentOp   string `json:"persistentOp"` // 持久化操作的名称
-	DstHash        string `json:"dstHash"`      // 生成的目标文件的 CID
-	outputFilePath string `json:"-"`            // 临时文件输出路径
+	Code         int    `json:"code"`         // 状态码。0 表示成功；1 表示失败
+	Desc         string `json:"desc"`         // 状态对应的描述
+	PersistentOp string `json:"persistentOp"` // 持久化操作的名称
 }
--- utils/getTmpDir.go
+++ utils/getTmpDir.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"log"
 	"os"
-	"path"
 	"path/filepath"
 )
 
@@ -17,25 +16,11 @@ func InitTmpDir() {
 			log.Fatalf("[ERROR] Create tmp dir failed [%v]", err)
 		}
 	}
-
-	_cacheDir := path.Join(_dir, "cache")
-	if !PathExists(_cacheDir) {
-		err := os.Mkdir(_cacheDir, os.ModePerm)
-		if err != nil {
-			log.Fatalf("[ERROR] Create cache dir failed, %v", err)
-		}
-	}
 }
 
 // 获取临时目录
 func GetTmpDir() string {
 	_dir, _ := filepath.Abs("./tmp")
-	return _dir
-}
-
-// 获取缓存目录
-func GetCacheDir() string {
-	_dir, _ := filepath.Abs("./tmp/cache")
 	return _dir
 }
 
